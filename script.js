76efbac50493cacb79a1cd1583f12452ef8fd14d
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-14 11:54:08 +0100

    fiddling with sizing the grid vs number of cells

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-const container = document.querySelector("#container");
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    container.appendChild(cell);
+  function createGrid(userNum, container) {
+    container.style.gridTemplateColumns = `repeat(${userNum}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${userNum}, 1fr)`;
+    for (let i = 0; i < userNum; i++) {
+      for (let j = 0; j < userNum; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+      }
+    }
+
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach((cell) => {
+      cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "blue";
+      });
+    });
   }
-}
 
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", () => {
-    cell.style.backgroundColor = "blue";
+  const numButton = document.querySelector("#numButton");
+  numButton.addEventListener("click", () => {
+    const userInput = prompt("Please enter a number between 0 and 100");
+    const userNum = parseInt(userInput);
+    if (isNaN(userNum) || userNum <= 0 || userNum >= 101) {
+      alert("Invalid input. Please enter a number between 0 and 100.");
+    } else {
+      createGrid(userNum, container);
+    }
   });
 });
-
-const numButton = document.querySelector("#numButton");
-numButton.addEventListener("click", ()=>{
-  const userInput = prompt("Please enetr a number between 0 and 100");
-  const userNum = parseInt(userInput);
-  if(isNaN(userNum || userNum <= 0 || userNum >= 101){
-    alert("Invalid input. Please enter a number between 0 and 100.");
-  }else{
-    //user the userNum for creating the grid.
-  })
-})
